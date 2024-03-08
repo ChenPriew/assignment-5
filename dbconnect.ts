78566074d4e066ca9f60ce4962de6e38a23a489a
ChenPriew <suriya0957329813@gmail.com>
2024-03-09 03:15:45 +0700

    Update dbconnect.ts

--- dbconnect.ts
+++ dbconnect.ts
@@ -2,8 +2,8 @@ import mysql from "mysql";
 
 export const conn = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "demo",
-  password: "abc123",
-  database: "imdb",
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6689713",
+  password: "n7BV4ur4WD",
+  database: "sql6689713",
 });
